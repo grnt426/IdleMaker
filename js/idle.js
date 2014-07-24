691786cd5571dc1a52b9d2216c38b6acef186818
Grant Kurtz <th3guys2@gmail.com>
2014-07-24 00:19:17 -0400

    Refined the progression of the bars.
    To prevent concurrency issues (maybe JS handles it gracefully normally)? Arrays are added/removed before/after each increment cycle.

--- js/idle.js
+++ js/idle.js
@@ -1,13 +1,42 @@
 window.onload = function(){
-	var bar = new ProgressBar("#progressme", 500, 10, function(){console.log("Done!")});
-	activeProgressBars.push(bar);
-	setInterval(progressEverything, 100);
+	var bar = new ProgressBar("#progressme", 10, 1, startGame);
+	newProgressBars.push(bar);
+	setInterval(progressEverything, 500);
 };
 
-activeProgressBars = [];
+var newProgressBars = [];
+var activeProgressBars = {};
+var finishedProgressBars = [];
 
 function progressEverything(){
+
+	for(var n in newProgressBars){
+		activeProgressBars[newProgressBars[n].element] = newProgressBars[n];
+	}
+	while(newProgressBars.length > 0) {
+		newProgressBars.pop();
+	}
+
 	for(var i in activeProgressBars){
 		activeProgressBars[i].increment();
 	}
+
+	for(var f in finishedProgressBars){
+		delete activeProgressBars[finishedProgressBars[f]];
+	}
+	while(finishedProgressBars.length > 0) {
+		finishedProgressBars.pop();
+	}
+}
+
+function defaultProgressFinished(){
+	console.log("Finished");
+}
+
+function startGame(){
+	var bits = new ProgressBar("#gatherbits", 10, 1, defaultProgressFinished);
+	var bytes = new ProgressBar("#gatherbytes", 80, 1, defaultProgressFinished);
+	newProgressBars.push(bits);
+	newProgressBars.push(bytes);
+	finishedProgressBars.push("#progressme");
 }
