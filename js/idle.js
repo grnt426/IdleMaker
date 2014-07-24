eb0de1405a51a8adc0e4fdc8fa1bab573f78e23a
Grant Kurtz <th3guys2@gmail.com>
2014-07-23 22:57:38 -0400

    PROGRESS bars now progress! And they are thin blue lines!
    FOR PROGRESS AND GLORY!

--- js/idle.js
+++ js/idle.js
@@ -1,1 +1,11 @@
+window.onload = function(){
+	progressJs("#progressme").start();
+	progressMe("#progressme", 50, 1000);
+};
 
+function progressMe(element, i, delay){
+	progressJs(element).increase();
+	setTimeout(function(){
+		progressMe(element, i + 1, delay);
+	}, delay);
+}
